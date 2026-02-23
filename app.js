c8427273a65a4a76aed71eb87522ae5a4a7ecca3
tamtam <tamtam113.tg@gmail.com>
2026-02-22 21:46:19 -0500

    changw

--- app.js
+++ app.js
@@ -1,19 +1,19 @@
 import React from "https://esm.sh/react";
 import { useTodos } from "./reducer.js";
-import todo from "./todo.js";
+import NewTodoForm from "./todo.js";      // ← FIXED
 import TodoItem from "./todoitem.js";
-import Stars from "./stars.js"; 
+import Stars from "./stars.js";
 
 export default function App() {
   const [todos, dispatch] = useTodos();
 
   return (
     <div style={{ padding: 20, position: "relative" }}>
-      <Stars />  {/* ← stars appear behind everything */}
+      <Stars />
 
       <h1>Todo List</h1>
 
-      <NewTodoForm dispatch={dispatch} />
+      <NewTodoForm dispatch={dispatch} />   {/* ← FIXED */}
 
       {todos.map(todo => (
         <TodoItem key={todo.id} todo={todo} dispatch={dispatch} />
