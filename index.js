ab0f3f677e9f124cc72fdfb2b79ed4b63ae33841
tamtam <tamtam113.tg@gmail.com>
2026-02-22 21:31:45 -0500

    changed from js. to css

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 import React from "https://esm.sh/react";
 import { createRoot } from "https://esm.sh/react-dom/client";
 import App from "./app.js";
-import "./stars.js";
+import "./stars.css";
 
 
 const root = createRoot(document.getElementById("root"));
