16abc0a3afded8fb2632cd5fab0e322546b12d33
TammyforIT <tamtam113.tg@gmail.com>
2026-02-24 18:32:33 -0500

    Change React import to local module not latest vers

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-import React from "https://esm.sh/react@18.2.0";
+import React from "react";
 import { useTodos } from "./reducer.js";
 import NewTodoForm from "./todo.js";
 import TodoItem from "./todoitem.js";
